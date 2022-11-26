7f27036189659f8e1a379588700a6c125eda89fb
atsumr <a@mrkm.sakura.ne.jp>
2022-11-26 17:47:08 +0900

    5.2.5 Step2

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -0,0 +1,93 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.Dispatchers
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class DefaultTasksRepositoryTest {
+
+    private val task1 = Task("Title1", "Description1")
+    private val task2 = Task("Title2", "Description2")
+    private val task3 = Task("Title3", "Description3")
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
+    private val localTasks = listOf(task3).sortedBy { it.id }
+    private val newTasks = listOf(task3).sortedBy { it.id }
+
+    private lateinit var tasksRemoteDataSource: FakeDataSource
+    private lateinit var tasksLocalDataSource: FakeDataSource
+
+    // Class under test
+    private lateinit var tasksRepository: DefaultTasksRepository
+
+    @Before
+    fun createRepository() {
+        tasksRemoteDataSource = FakeDataSource(remoteTasks.toMutableList())
+        tasksLocalDataSource = FakeDataSource(localTasks.toMutableList())
+        // Get a reference to the class under test
+        tasksRepository = DefaultTasksRepository(
+            // TODO Dispatchers.Unconfined should be replaced with Dispatchers.Main
+            //  this requires understanding more about coroutines + testing
+            //  so we will keep this as Unconfined for now.
+            tasksRemoteDataSource, tasksLocalDataSource, Dispatchers.Unconfined
+        )
+    }
+
+    @Test
+    fun getTasks() {
+    }
+
+    @Test
+    fun refreshTasks() {
+    }
+
+    @Test
+    fun observeTasks() {
+    }
+
+    @Test
+    fun refreshTask() {
+    }
+
+    @Test
+    fun observeTask() {
+    }
+
+    @Test
+    fun getTask() {
+    }
+
+    @Test
+    fun saveTask() {
+    }
+
+    @Test
+    fun completeTask() {
+    }
+
+    @Test
+    fun testCompleteTask() {
+    }
+
+    @Test
+    fun activateTask() {
+    }
+
+    @Test
+    fun testActivateTask() {
+    }
+
+    @Test
+    fun clearCompletedTasks() {
+    }
+
+    @Test
+    fun deleteAllTasks() {
+    }
+
+    @Test
+    fun deleteTask() {
+    }
+}
