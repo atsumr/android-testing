defb2ab2fdb145fb79be8b910f6339ebb0ad836c
atsumr <a@mrkm.sakura.ne.jp>
2022-11-27 10:04:50 +0900

    5.2.6 Step3

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeTestRepository.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeTestRepository.kt
@@ -1,20 +1,34 @@
 package com.example.android.architecture.blueprints.todoapp.data.source
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.android.architecture.blueprints.todoapp.data.Result
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.runBlocking
 
-class FakeTestRepository  : TasksRepository  {
+class FakeTestRepository  : TasksRepository {
+
+    var tasksServiceData: LinkedHashMap<String, Task> = LinkedHashMap()
+
+    private val observableTasks = MutableLiveData<Result<List<Task>>>()
+
     override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>> {
-        TODO("Not yet implemented")
+        return Result.Success(tasksServiceData.values.toList())
     }
 
     override suspend fun refreshTasks() {
-        TODO("Not yet implemented")
+        observableTasks.value = getTasks()
     }
 
     override fun observeTasks(): LiveData<Result<List<Task>>> {
-        TODO("Not yet implemented")
+        runBlocking { refreshTasks() }
+        return observableTasks
+    }
+
+    override suspend fun completeTask(task: Task) {
+        val completedTask = task.copy(isCompleted = true)
+        tasksServiceData[task.id] = completedTask
+        refreshTasks()
     }
 
     override suspend fun refreshTask(taskId: String) {
@@ -30,10 +44,6 @@ class FakeTestRepository  : TasksRepository  {
     }
 
     override suspend fun saveTask(task: Task) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun completeTask(task: Task) {
         TODO("Not yet implemented")
     }
 
@@ -60,4 +70,5 @@ class FakeTestRepository  : TasksRepository  {
     override suspend fun deleteTask(taskId: String) {
         TODO("Not yet implemented")
     }
+
 }
